b1087ac72cd43ea2623c8bdacec40f282e0728c5
FeyoMx <elfeyo1980@gmail.com>
2017-05-30 10:28:57 -0500

    Update and rename x510-vendor-blobs.mk to br6580_weg_emmc_m-vendor-blobs.mk
    Cagabi-fy

---
+++ br6580_weg_emmc_m-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cagabi/br6580_weg_emmc_m/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cagabi/br6580_weg_emmc_m/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cagabi/br6580_weg_emmc_m/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cagabi/br6580_weg_emmc_m/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cagabi/br6580_weg_emmc_m/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cagabi/br6580_weg_emmc_m/vendor,system/vendor)
